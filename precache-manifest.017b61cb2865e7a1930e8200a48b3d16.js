88d290290fcc29d64fa02659c7a844134429b8a9
holubitskaya anastasiya <nastya030477@gmail.com>
2020-08-19 11:15:53 +0300

    Updates

---
+++ precache-manifest.017b61cb2865e7a1930e8200a48b3d16.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c77d42b53d3e4ddf0b8c0546b46a2e",
+    "url": "/demo-socialChef/index.html"
+  },
+  {
+    "revision": "1b9d067e86027aee3312",
+    "url": "/demo-socialChef/static/css/3.7a5665ab.chunk.css"
+  },
+  {
+    "revision": "a4d1466282ca4a27cd28",
+    "url": "/demo-socialChef/static/css/main.e5f8e3f7.chunk.css"
+  },
+  {
+    "revision": "8c66eace3790f10e71c0",
+    "url": "/demo-socialChef/static/js/2.7206cc14.chunk.js"
+  },
+  {
+    "revision": "ef3c8464c45419da4d5bb3ad62cf7f71",
+    "url": "/demo-socialChef/static/js/2.7206cc14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b9d067e86027aee3312",
+    "url": "/demo-socialChef/static/js/3.ee0b1f5d.chunk.js"
+  },
+  {
+    "revision": "a4d1466282ca4a27cd28",
+    "url": "/demo-socialChef/static/js/main.aef6a5a3.chunk.js"
+  },
+  {
+    "revision": "a7fb3ccb906f7347ff83",
+    "url": "/demo-socialChef/static/js/runtime-main.6ba408b3.js"
+  },
+  {
+    "revision": "00c902b669a743f20da0368fd777f4e4",
+    "url": "/demo-socialChef/static/media/327.00c902b6.gif"
+  },
+  {
+    "revision": "6b562d7ca359ccb3259f570bcceb4f70",
+    "url": "/demo-socialChef/static/media/Raleway-Light.6b562d7c.ttf"
+  },
+  {
+    "revision": "b3748400a085263d6242ee2f9f89891e",
+    "url": "/demo-socialChef/static/media/body.b3748400.jpg"
+  },
+  {
+    "revision": "d5f63460022093bb31faeaf9fae11bd5",
+    "url": "/demo-socialChef/static/media/facebook_101791.d5f63460.svg"
+  },
+  {
+    "revision": "bd4cd744ac56e2b2f422fdd58c366f10",
+    "url": "/demo-socialChef/static/media/github_101792.bd4cd744.svg"
+  },
+  {
+    "revision": "2ecea040b42fc9a6a0987c927ad59989",
+    "url": "/demo-socialChef/static/media/instagram_101780.2ecea040.svg"
+  },
+  {
+    "revision": "b46d675c0f8298b67c376e1426a48b4e",
+    "url": "/demo-socialChef/static/media/photo_2020-07-26_23-35-01.b46d675c.jpg"
+  },
+  {
+    "revision": "e39bc4848a65d9824c44d2404777ce69",
+    "url": "/demo-socialChef/static/media/twitter_101809.e39bc484.svg"
+  },
+  {
+    "revision": "bcd94337b42fc620c1aac15a07ebcef7",
+    "url": "/demo-socialChef/static/media/vk_101783.bcd94337.svg"
+  },
+  {
+    "revision": "431e6724346993af037da310f11389bf",
+    "url": "/demo-socialChef/static/media/youtube_101764.431e6724.svg"
+  }
+]);
